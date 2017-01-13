8bed7bbadb4f62ead7755d2841a03ffbf1a664a5
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-12 22:35:10 -0200

    Added method freeze

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,11 +1,24 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-       this.data = new Date();
-       this.quantidade = quantidade;
-       this.valor = valor;
+       this._data = new Date();
+       this._quantidade = quantidade;
+       this._valor = valor;
+       Object.freeze(this); //congela a instancia do objeto
       }
 
-  obtemVolume(){
-    return this.quantidade * this.valor;
+  get volume(){
+    return this._quantidade * this._valor;
+  }
+
+  get data(){
+    return this._data;
+  }
+
+  get quantidade(){
+    return this._quantidade;
+  }
+
+  get valor(){
+    return this._valor;
   }
 }
