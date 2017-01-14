ef0296ac5643dab0fc0f596bb8bad399d1886835
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-14 18:00:23 -0200

    Created Controller

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,6 +1,6 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-       this._data = new Date();
+       this._data = new Date(data.getTime());
        this._quantidade = quantidade;
        this._valor = valor;
        Object.freeze(this); //congela a instancia do objeto
@@ -11,7 +11,7 @@ class Negociacao {
   }
 
   get data(){
-    return this._data;
+    return new Date(this._data.getTime());
   }
 
   get quantidade(){
