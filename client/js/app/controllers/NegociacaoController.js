1289956aa76aeb128e98054caf3fde13948552b5
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-18 18:56:25 -0200

    Solving a problem with the functional paradigm

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,18 +3,40 @@ class NegociacaoController {
     constructor() {
 
      let $ = document.querySelector.bind(document);
-     this.inputData = $('#data');
-     this.inputQuantidade = $('#quantidade');
-     this.inputValor = $('#valor');
+     this._inputData = $('#data');
+     this._inputQuantidade = $('#quantidade');
+     this._inputValor = $('#valor');
 
     }
 
    adiciona(event){
      event.preventDefault();
 
-     console.log(this.inputData.value);
-     console.log(this.inputQuantidade.value);
-     console.log(this.inputValor.value);
+     console.log(typeof(this._inputData.value));
+
+     let data = new Date(...
+         this._inputData.value
+         .split('-')
+         .map(function(item,indice){
+            if(indice == 1){
+                return item - 1;
+            }
+            return item;
+         })
+
+     );
+
+     console.log(data);
+
+    // let negociacao = new Negociacao(
+    //   this._inputData.value,
+    //   this._inputQuantidade.value,
+    //   this._inputValor.value
+    // );
+
+      // console.log(negocicao);
+      //Adicionar a negocicaco em uma lista
+
    }
 
 }
