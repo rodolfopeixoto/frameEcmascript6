d702128df33fb04f9e6fe529db4b3219ff1ade0e
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-18 18:33:23 -0200

    Avoid traversing DOM

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,8 +1,20 @@
 class NegociacaoController {
 
+    constructor() {
+
+     let $ = document.querySelector.bind(document);
+     this.inputData = $('#data');
+     this.inputQuantidade = $('#quantidade');
+     this.inputValor = $('#valor');
+
+    }
+
    adiciona(event){
      event.preventDefault();
-     alert('Chamei ação no controller');
+
+     console.log(this.inputData.value);
+     console.log(this.inputQuantidade.value);
+     console.log(this.inputValor.value);
    }
 
 }
